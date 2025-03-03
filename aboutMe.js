97a2ec4d681f3c458922fdfa7dff28bd00e84a09
Jreev <jacob.reevcrona@yahoo.com>
2025-03-03 11:54:48 +0100

    done with assignment

--- aboutMe.js
+++ aboutMe.js
@@ -1,1 +1,38 @@
-console.log("yo");
+const printName = (callback) => {
+  setTimeout(() => {
+    console.log("My name is Jacob");
+    console.log("(2 second delay)");
+    callback();
+  }, 2000);
+};
+const printAge = (callback) => {
+  setTimeout(() => {
+    console.log("I am 26 years old");
+    console.log("(3 second delay)");
+    callback();
+  }, 3000);
+};
+const printBirthPlace = (callback) => {
+  setTimeout(() => {
+    console.log("I was born in Helsingborg");
+    console.log("(4 second delay)");
+    callback();
+  }, 4000);
+};
+const printHobbies = (callback) => {
+  setTimeout(() => {
+    console.log("My hobbies are playing video games, watching mma and coding");
+    console.log("(5 second delay)");
+    callback();
+  }, 5000);
+};
+
+printName(() => {
+  printAge(() => {
+    printBirthPlace(() => {
+      printHobbies(() => {
+        console.log("That's some information about me");
+      });
+    });
+  });
+});
